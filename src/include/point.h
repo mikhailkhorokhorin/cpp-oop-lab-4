4246621b50f4cb5e5cf792988c5ba9dc7e84498d
mikhailkhorokhorin <mikhailkhorokhorin@gmail.com>
2025-10-26 11:19:56 +0300

    Finish lab

--- src/include/point.h
+++ src/include/point.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include <type_traits>
+using namespace std;
 
 template <typename T>
 concept Scalar = std::is_arithmetic_v<T>;
@@ -8,16 +10,20 @@ template <Scalar T>
 struct Point {
     T x{}, y{};
 
-    Point();
-    Point(T x_, T y_);
-
-    bool operator==(const Point& other) const;
-
-    friend std::istream& operator>>(std::istream& is, Point& p) {
-        return is >> p.x >> p.y;
+    Point() : x(0), y(0) {
+    }
+    Point(T x_, T y_) : x(x_), y(y_) {
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Point& p) {
-        return os << "(" << p.x << ", " << p.y << ")";
+    bool operator==(const Point& other) const {
+        return x == other.x && y == other.y;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Point& point) {
+        return is >> point.x >> point.y;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Point& point) {
+        return os << "(" << point.x << ", " << point.y << ")";
     }
 };
